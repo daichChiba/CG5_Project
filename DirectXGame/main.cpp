d2fabdc0d453588547096f5abb7f8c5864063820
LC1D_17_Chiba_Daichi <146914435+daichChiba@users.noreply.github.com>
2025-04-23 10:27:08 +0900

    00_02の内容が終わったがブランチを切っていないことがわかったため一旦プッシュし内容を消す

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -3,6 +3,33 @@
 #include <Windows.h>
 
 using namespace KamataEngine;
+
+// シェイダーコンパイル関数
+// filePath: シェーダファイルのパス 例L"Resources/Shaders/TestVS.hlsl"
+// shaderModel: シェイダーモデル 例"vs_5_0"
+Microsoft::WRL::ComPtr<ID3DBlob> CompileShader(const std::wstring& filePath, const std::string& shaderModel) {
+	Microsoft::WRL::ComPtr<ID3DBlob> shaderBlob = nullptr;
+	Microsoft::WRL::ComPtr<ID3DBlob> errorBlob = nullptr;
+	HRESULT hr = D3DCompileFromFile(
+	    filePath.c_str(),                                // シェーダファイル名
+	    nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE,      // インクルード可能になる
+	    "main", shaderModel.c_str(),                     // エントリーポイント名、シェーダモデル
+	    D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバッグ用設定
+	    0, &shaderBlob, &errorBlob);
+	// エラーが発生した場合、止める
+	if (FAILED(hr)) {
+		if (errorBlob) {
+			DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<const char*>(errorBlob->GetBufferPointer()));
+		}
+		assert(false);
+	}
+	// 生成したshaderBlobを返す
+	return shaderBlob;
+}
+
+// 関数プロトタイプ宣言
+Microsoft::WRL::ComPtr<ID3DBlob> CompileShader(const std::wstring& filePath, const std::string& shaderModel);
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// エンジンの初期化
@@ -26,11 +53,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	Microsoft::WRL::ComPtr<ID3DBlob> signatureBlob = nullptr;
 	Microsoft::WRL::ComPtr<ID3DBlob> errorBlob = nullptr;
 	HRESULT hr = D3D12SerializeRootSignature(&descriptionRootSignature, D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlob);
-	if (FAILED(hr)) {
-		DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<const char*>(errorBlob->GetBufferPointer()));
-		assert(false);
-	}
-	// バイナリを基に作成
+	//  バイナリを基に作成
 	Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature = nullptr;
 	hr = dxCommon->GetDevice()->CreateRootSignature(0, signatureBlob->GetBufferPointer(), signatureBlob->GetBufferSize(), IID_PPV_ARGS(&rootSignature));
 	assert(SUCCEEDED(hr));
@@ -56,40 +79,13 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// 塗りつぶしモードをソリッドにする(ワイヤーフレームならD3D12_FILL_MODE_WIREFRAME)
 	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
 
-	// コンパイル済みのShader、エラー次情報の格納場所の用意
-	Microsoft::WRL::ComPtr<ID3DBlob> vsBlob = nullptr;    // 頂点シェーダオブジェクト
-	Microsoft::WRL::ComPtr<ID3DBlob> psBlob = nullptr;    // ピクセルシェーダオブジェクト
-	// 頂点シェーダの読み込みとコンパイル
-	std::wstring vsFile = L"Resources/Shaders/TestVS.hlsl";
-	hr = D3DCompileFromFile(
-	    vsFile.c_str(),                                  // シェーダファイル名
-	    nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE,      // インクルード可能になる
-	    "main", "vs_5_0",                                // エントリーポイント名、シェーダモデル
-	    D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバッグ用設定
-	    0, &vsBlob, &errorBlob);
-	if (FAILED(hr)) {
-		DebugText::GetInstance()->ConsolePrintf(std::system_category().message(hr).c_str());
-		if (errorBlob) {
-			DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<const char*>(errorBlob->GetBufferPointer()));
-			assert(false);
-		}
-	}
-	// ピクセルシェーダの読み込みとコンパイル
-	std::wstring psFile = L"Resources/Shaders/TestPS.hlsl";
-	hr = D3DCompileFromFile(
-	    psFile.c_str(), // シェーダファイル名
-	    nullptr,
-	    D3D_COMPILE_STANDARD_FILE_INCLUDE,               // インクルード可能になる
-	    "main", "ps_5_0",                                // エントリーポイント名、シェーダモデル
-	    D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバッグ用設定
-	    0, &psBlob, &errorBlob);
-	if (FAILED(hr)) {
-		DebugText::GetInstance()->ConsolePrintf(std::system_category().message(hr).c_str());
-		if (errorBlob) {
-			DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<const char*>(errorBlob->GetBufferPointer()));
-			assert(false);
-		}
-	}
+	// 頂点シェイダーの読み込みとコンパイル
+	Microsoft::WRL::ComPtr<ID3DBlob> vsBlob = CompileShader(L"Resources/Shaders/TestVS.hlsl", "vs_5_0");
+	assert(vsBlob != nullptr);
+	// ピクセルシェイダーの読み込みとコンパイル
+	Microsoft::WRL::ComPtr<ID3DBlob> psBlob = CompileShader(L"Resources/Shaders/TestPS.hlsl", "ps_5_0");
+	assert(psBlob != nullptr);
+
 	// PSO(PipelineStateObject)の作成 -----------
 	D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPipelineStateDesc{};
 	graphicsPipelineStateDesc.pRootSignature = rootSignature.Get();                       // RootSignature
@@ -118,7 +114,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// 頂点リソースの設定
 	D3D12_RESOURCE_DESC vertexResourceDesc{};
 	vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER; // バッファ
-	vertexResourceDesc.Width = sizeof(Vector4) * 3;// リソースのサイズ。今回はVector4を3頂点分
+	vertexResourceDesc.Width = sizeof(Vector4) * 3;                 // リソースのサイズ。今回はVector4を3頂点分
 	// バッファの場合はこれらは1にする決まり
 	vertexResourceDesc.Height = 1;
 	vertexResourceDesc.DepthOrArraySize = 1;
@@ -132,7 +128,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	    &uploadHeapProperties, D3D12_HEAP_FLAG_NONE,            // ヒープの設定
 	    &vertexResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, // リソースの設定
 	    nullptr, IID_PPV_ARGS(&vertexResource));                // リソースの生成
-	assert(SUCCEEDED(hr)); //うまくいかなかったときは起動できない
+	assert(SUCCEEDED(hr));                                      // うまくいかなかったときは起動できない
 
 	// VertexBufferViewを作成する -----------
 	D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
@@ -165,7 +161,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 		// ここに描画処理を記述する
 
-		//コマンドを積む
+		// コマンドを積む
 		commandList->SetGraphicsRootSignature(rootSignature.Get()); // RootSignatureの設定
 		commandList->SetPipelineState(pipelineState.Get());         // PSOの設定をする
 		commandList->IASetVertexBuffers(0, 1, &vertexBufferView);   // VBVの設定をする
